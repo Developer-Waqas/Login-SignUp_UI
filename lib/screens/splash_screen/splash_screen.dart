2521e96d39c3d67ff990090007d8fda7b0e42b40
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-01-29 21:02:05 +0500

    splash screen

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -16,9 +16,30 @@ class SplashScreen extends StatelessWidget {
             colors: [primaryGradColor, secondaryGradColor],
           ),
           image: DecorationImage(
-            image: AssetImage('assets/images/img_splash_img.png'),
-            scale: 3
-          ),
+              image: AssetImage('assets/images/img_splash_img.png'), scale: 3),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 150,
+            ),
+            Center(
+              child: Text(
+                'aliments',
+                style: TextStyle(
+                  fontFamily: 'PoppinsBold',
+                  color: Colors.white,
+                  fontSize: 35
+                ),
+              ),
+            ),
+            Text('Food Delivery Services',style: TextStyle(
+              fontSize: 16,
+              fontFamily: 'PoppinsRegular',
+              color: Colors.white,
+            ),),
+          ],
         ),
       ),
     );
