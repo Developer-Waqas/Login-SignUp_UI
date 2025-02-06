3e4e46a5cbba50e92369cc392db0979a3e87d517
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-02-06 22:23:12 +0500

    Initial commit

--- lib/constants/image_strings/app_image_assets.dart
+++ lib/constants/image_strings/app_image_assets.dart
@@ -1,7 +1,7 @@
 class AppImageAssets {
   static const String imageBasePath = 'assets/images/';
   static const String splashImage = '${imageBasePath}img_spalsh.png';
-  static const String onboardingImage1 = '${imageBasePath}img_onboarding.png';
+  static const String onboardingImage1 = '${imageBasePath}img_onboarding1.png';
   static const String onboardingImage2 = '${imageBasePath}img_onboarding2.png';
   static const String onboardingImage3 = '${imageBasePath}img_onboarding3.png';
   static const String googleIcon = '${imageBasePath}img_google_logo.png';
